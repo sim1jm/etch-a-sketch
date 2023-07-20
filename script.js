ac8a5415de98d259ddfbcbc8c62055b06d937dc8
sim1jm <simmonejairusmanuel@gmail.com>
2023-07-20 19:40:08 +0300

    Create grid, code functionality
    No change size function yet

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-//Grid Creation
+// Grid Creation
 const height = 48;
-const width = height;
-const border = 2;
+const border = 1;
 
 const divGrid = document.querySelector('div.grid');
-const divSquares = document.querySelectorAll('div.squares')
 
 divGrid.style.borderWidth = `${border}px`;
-divGrid.style.height = `${(height + 2 * border) * 16}px`;
-divGrid.style.width = `${(height + 2 * border) * 16}px`;
+divGrid.style.height = `${(height + 2 * border) * 16}px`; // should be constant
+divGrid.style.width = divGrid.style.height;
 
-for (let i = 1; i <= 256; i++) {
+
+for (let i = 1; i <= 16 ** 2; i++) {
     const divSquares = document.createElement('div');
     
     divSquares.style.height = `${height}px`;
     divSquares.style.width = divSquares.style.height; 
     divSquares.style.borderWidth = `${border}px`;
     divSquares.classList.add('squares');
-    
+
     divGrid.appendChild(divSquares); 
 }
 
-//Etch-a-Sketch
+// Etch-a-Sketch
+const divSquares = Array.from(document.querySelectorAll('div.squares'));
+divSquares.forEach(square => square.addEventListener('mouseover', changeBGColor));
+
+function changeBGColor() {
+    this.style.backgroundColor = 'blue';
+}
+
+
+
+
 
